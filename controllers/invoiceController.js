3e735d7338dbc319aa99ac22dd417a96446ce735
Achref LAABIDI <abidi.achref95@gmail.com>
2023-09-23 18:20:27 +0100

    invoice routes

--- controllers/invoiceController.js
+++ controllers/invoiceController.js
@@ -4,11 +4,22 @@ const generateInvoice = async (req, res, next) => {
     try {
         const { product, price, quantity } = req.body
         if (product === "" || price === ""|| quantity === "") {
-            res.status(400).json({ message: "Provide product, price and quantity." });
-            return;
+            return res.status(400).json({ message: "Provide product, price and quantity." });
           }
         await Invoice.create({product, price, quantity })
         return res.status(200).json({ message: "invoice created and stored"})
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+const getInvoice = async (req, res, next ) => {
+    try {
+        const invoice = await Invoice.find()
+        if (!invoice) {
+            return res.status(400).json({message : "No invoices found"})
+        }
+        return res.status(200).json({invoice: invoice})
     } catch (error) {
         console.log(error)
     }
@@ -16,4 +27,5 @@ const generateInvoice = async (req, res, next) => {
 
 module.exports = {
     generateInvoice,
+    getInvoice,
 }
--- routes/invoice.routes.js
+++ routes/invoice.routes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { isAuthenticated } = require("../middleware/jwt.middleware");
-const { generateInvoice } = require("../controllers/invoiceController");
+const { 
+        generateInvoice,
+        getInvoice
+        } = require("../controllers/invoiceController");
 
-router.post("/generate", isAuthenticated, generateInvoice)
+router.post("/generate", isAuthenticated, generateInvoice);
+router.get("/get", isAuthenticated, getInvoice);
 
 module.exports = router;
