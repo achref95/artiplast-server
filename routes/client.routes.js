cd36c4f5ee8c926665f50877f0f7a09d102006e2
Achref LAABIDI <abidi.achref95@gmail.com>
2023-10-21 15:15:31 +0100

    client controller improvements

--- routes/client.routes.js
+++ routes/client.routes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const { isAuthenticated } = require("../middleware/jwt.middleware");
 const { getClients, addClient, getAll } = require("../controllers/clientController");
 
-router.get("/all", getAll);
+router.get("/all", isAuthenticated, getAll);
 router.get("/get", isAuthenticated, getClients);
-router.post("/add", addClient);
+router.post("/add", isAuthenticated, addClient);
 // router.get("/:id", getClient);
 
 
